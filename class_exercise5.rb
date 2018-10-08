0385c2c026c94e3ba7f6ffc674422fd912d4a7a1
Lina Gallardo <lina.gallardo@koombea.com>
2018-10-08 14:48:34 -0500

    Class of the factorial numbers

---
+++ class_exercise5.rb
@@ -0,0 +1,16 @@
+#As an engineer, I must create a method that receives any number and returns the Factorial of that number.
+
+class FactorialNumber
+  def initialize(num)
+    @num = num
+  end 
+  def factorial 
+    @num.zero? ? "1" : @num.downto(1).inject(:*)
+  end
+end 
+
+
+print "Enter a number: "
+num = gets.chomp.to_i 
+number = FactorialNumber.new(num)
+puts "The factorial number of #{ num } is #{ number.factorial } "
