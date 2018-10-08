a2fc3b73d350b017b40fc4c5a1a4d6425c467674
Lina Gallardo <lina.gallardo@koombea.com>
2018-10-08 14:27:30 -0500

    Class of the numbers palindromic

---
+++ class_exercise4.rb
@@ -0,0 +1,22 @@
+# A palindromic number reads the same both ways. The largest palindrome made from the product of
+#two 2-digit numbers is 9009 = 91 × 99.
+
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+class Palindromo
+  def initialize(limit)
+    @limit = limit
+  end
+  def palindromic
+    numbers = []
+    100.upto(@limit).each do |num_1|
+      (100..@limit).each do |num_2|
+        numbers << (num_1 * num_2)
+      end
+    end
+    numbers.select { |num| num.to_s.eql?(num.to_s.reverse) }.max
+  end
+end
+
+number = Palindromo.new(999)
+puts number.palindromic
